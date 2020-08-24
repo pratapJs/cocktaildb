ecaef5f84ea77b0e818a76416865feca90e619ba
savip <pratapadhikari81@gmail.com>
2020-08-24 20:45:59 +0930

    about page edited

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,13 +5,14 @@ const About = () => {
     <section className='section about-section'>
       <h1 className='section-title'>about us</h1>
       <p>
-        Labore voluptate ipsum proident aliqua enim culpa.Magna enim et occaecat
-        qui sit nisi sint cupidatat nulla in deserunt. Velit ullamco enim
-        cupidatat incididunt pariatur nisi labore nisi excepteur pariatur anim
-        tempor. Adipisicing voluptate veniam excepteur adipisicing. Aute ipsum
-        in ipsum aliqua sint excepteur ipsum elit ut esse. Proident quis fugiat
-        excepteur ad deserunt commodo. Sunt anim sint Lorem eiusmod sint velit
-        occaecat sint anim labore est sint.
+        Hello everyone. Welcome to my Site. My name is Pratap Adhikari. I am
+        from Darwin, NT, Australia. I love to make cocktails. I first tried to
+        make cocktail when i was 15 years old. This website is all about
+        cocktails and the methods to make the cocktails. I have tried my best to
+        make it simple and effective. Thanks to Saveena, Saroja and Sujata for
+        helping me in this project. Hope you will like it. Please donot forget
+        to provide me feedback @ pratapadhikari81@gmail.com. Thank you.
+        Cheers!!!
       </p>
     </section>
   );
